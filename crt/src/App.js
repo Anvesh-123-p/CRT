485502f72fd984112756637a6063379e0cf73859
mounish <mounishv04@gmail.com>
2024-09-15 19:28:35 +0530

    Approval_Data component created

--- crt/src/App.js
+++ crt/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Hod_Edit from './components/Hod/Hod_Edit/Hod_Edit';
+import Approval_New_Account from './components/Approval_Data/Approval_New_Account/Approval_New_Account';
+import Approval_Lesson_Plan from './components/Approval_Data/Approval_Lesson_Plan/Approval_Lesson_Plan';
 import { BrowserRouter as Router,Routes , Route, BrowserRouter } from 'react-router-dom';
 import React, {useState} from 'react';
 
@@ -9,6 +11,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/Hodedit" element={<Hod_Edit/>} />
+        <Route path="/approval_act" element={<Approval_New_Account/>} />
+        <Route path="/approval_lesson" element={<Approval_Lesson_Plan/>} />
       </Routes>
     </Router>
   );
