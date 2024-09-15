4859d4ac2224a6132eec0d930e5a0b555bac4bf3
srikanth <msrikanth9346@gmail.com>
2024-09-15 19:09:17 +0530

    Approval page is created

--- crt/src/App.js
+++ crt/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Hod_Edit from './components/Hod/Hod_Edit/Hod_Edit';
 import Subject_view from './components/Faculty/Faculty_Subject_view/Subject_view';
 import Lesson_plan from './components/Faculty/Faculty_lesson_plan/Lesson_plan';
+import Approval from './components/Faculty/Faculty_Approval/Approval';
 import { BrowserRouter as Router,Routes , Route, BrowserRouter } from 'react-router-dom';
 import React, {useState} from 'react';
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="/Hodedit" element={<Hod_Edit/>} />
         <Route path="/Subject_view" element={<Subject_view/>} />
         <Route path="/Lesson_plan" element={<Lesson_plan/>} />
+        <Route path="/Approval" element={<Approval/>} />
       </Routes>
     </Router>
   );
