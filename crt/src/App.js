95fffe9da867a30afd158405960bd958e69642d6
srikanth <msrikanth9346@gmail.com>
2024-09-15 18:16:56 +0530

    Subject_view is created

--- crt/src/App.js
+++ crt/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Hod_Edit from './components/Hod/Hod_Edit/Hod_Edit';
+import Subject_view from './components/Faculty/Faculty_Subject_view/Subject_view';
 import { BrowserRouter as Router,Routes , Route, BrowserRouter } from 'react-router-dom';
 import React, {useState} from 'react';
 
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/Hodedit" element={<Hod_Edit/>} />
+        <Route path="/Subject_view" element={<Subject_view/>} />
       </Routes>
     </Router>
   );
