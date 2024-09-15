223c34fc1c13586c3c486f6dc3c7ff2629caedf8
srikanth <msrikanth9346@gmail.com>
2024-09-15 18:40:40 +0530

    Lesson

--- crt/src/App.js
+++ crt/src/App.js
@@ -2,8 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Hod_Edit from './components/Hod/Hod_Edit/Hod_Edit';
 import Subject_view from './components/Faculty/Faculty_Subject_view/Subject_view';
+import Lesson_plan from './components/Faculty/Faculty_lesson_plan/Lesson_plan';
 import { BrowserRouter as Router,Routes , Route, BrowserRouter } from 'react-router-dom';
 import React, {useState} from 'react';
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/Hodedit" element={<Hod_Edit/>} />
         <Route path="/Subject_view" element={<Subject_view/>} />
+        <Route path="/Lesson_plan" element={<Lesson_plan/>} />
       </Routes>
     </Router>
   );
