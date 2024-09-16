4a6a56b1932824ee9efa476043a980c26841a3fc
kiran <kirankumar25577@gmail.com>
2024-09-16 19:57:30 +0530

    Hod Home component is added

--- crt/src/App.js
+++ crt/src/App.js
@@ -4,6 +4,9 @@ import Hod_Edit from './components/Hod/Hod_Edit/Hod_Edit';
 import Subject_view from './components/Faculty/Faculty_Subject_view/Subject_view';
 import Lesson_plan from './components/Faculty/Faculty_lesson_plan/Lesson_plan';
 import Approval from './components/Faculty/Faculty_Approval/Approval';
+import Hod_Home from './components/Hod/Hod_Home/Hod_Home';
+import Forgot_Pass from './components/Hod/Forgot_Pass/Forgot_Pass';
+import Verify from './components/Hod/Verify/Verify';
 import { BrowserRouter as Router,Routes , Route, BrowserRouter } from 'react-router-dom';
 import React, {useState} from 'react';
 
@@ -13,6 +16,9 @@ function App() {
     <Router>
       <Routes>
         <Route path="/Hodedit" element={<Hod_Edit/>} />
+        <Route path="/Hodhome" element={<Hod_Home/>} />
+        <Route path="/verify" element={<Verify/>} />
+        <Route path="/forgot" element={<Forgot_Pass/>} />
         <Route path="/Subject_view" element={<Subject_view/>} />
         <Route path="/Lesson_plan" element={<Lesson_plan/>} />
         <Route path="/Approval" element={<Approval/>} />
